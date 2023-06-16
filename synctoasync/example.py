b0ffc23c0be98a89dc625a499a992c02951b9a4a
Alborz <45658572+ababaiem@users.noreply.github.com>
2023-06-16 00:24:39 +0000

    Basic example of async scraping script

---
+++ synctoasync/example.py
@@ -0,0 +1,19 @@
+import httpx
+import asyncio
+from rich import print
+
+async def get_data(client, url):
+    resp = await client.get(url)
+    return resp.json()['name']
+
+async def main():
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        for i in range(1, 150):
+            tasks.append(get_data(client, f"https://rickandmortyapi.com/api/character/{i}"))
+
+        characters = await asyncio.gather(*tasks)
+        for c in characters:
+            print(c)
+
+asyncio.run(main())
